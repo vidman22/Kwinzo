0ccb935a55d1852e9bdd1807638c43a2b761b348
vidman22 <jvidmar37@gmail.com>
2018-09-05 15:39:23 -0700

    index relative path catcher

--- server/index.js
+++ server/index.js
@@ -25,7 +25,16 @@ app.use('/graphql', graphqlHTTP({
   graphiql:true
 }));
 
-app.use(express.static(path.join(__dirname, '../build')));
+// app.use(express.static(path.join(__dirname, '../build')));
+
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname, '../build/index.html'), function(err) {
+    if (err) {
+      res.status(500).send(err)
+    }
+  })
+})
+
 
 /*
 app.use(cors({
