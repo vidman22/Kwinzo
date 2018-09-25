38894c963b610133765fb9218510fe8d7cec4ffe
Ashish Thakwani <athakwani@gmail.com>
2018-09-25 19:40:37 +0000

    Added path dependency

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const server = require('http').Server(app);
 const passport = require('passport');
 const graphqlHTTP = require('express-graphql');
