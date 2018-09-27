27bb7e8d663dcaa5330a8417cf3672381890a888
vidman22 <jvidmar37@gmail.com>
2018-09-26 21:05:47 -0700

    server

--- server/index.js
+++ server/index.js
@@ -26,20 +26,20 @@ app.use('/graphql', graphqlHTTP({
   graphiql:true
 }));
 
-// app.use(express.static(path.join(__dirname, '../build')));
+app.use(express.static(path.join(__dirname, '../build')));
 
-// app.get('/*', function(req, res) {
-//   res.sendFile(path.join(__dirname, '../build/index.html'), function(err) {
-//     if (err) {
-//       res.status(500).send(err)
-//     }
-//   })
-// })
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname, '../build/index.html'), function(err) {
+    if (err) {
+      res.status(500).send(err)
+    }
+  })
+})
 
 
-app.use(cors({
-	origin: 'http://localhost:3000'
-}));
+// app.use(cors({
+// 	origin: 'http://localhost:3000'
+// }));
 
 
 server.listen(PORT, () => {
